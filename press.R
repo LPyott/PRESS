f3838715b6adcfcd18a68129053017d41de18a05
Pyott <LPYOTT@wcupa.edu>
2020-06-08 08:42:57 -0400

    cleaned up comments

--- press.R
+++ press.R
@@ -1,7 +1,7 @@
 setwd("C:/Users/75LPYOTT/Desktop")
 
-data <- read.csv("scott_nover_data.csv", header=TRUE)
-head(data)
+#data <- read.csv("scott_nover_data.csv", header=TRUE)
+#head(data)
 
 
 #https://github.com/wmurphyrd/fiftystater
@@ -10,11 +10,11 @@ head(data)
 #https://eriqande.github.io/rep-res-eeb-2017/map-making-in-R.html
 #https://stackoverflow.com/questions/34301835/how-to-color-states-in-us-map-in-r
 
-install.packages("mapdata")
-install.packages("devtools")
-install.packages("stringr")
-install.packages("maps")
-devtools::install_github("wmurphyrd/fiftystater")
+#install.packages("mapdata")
+#install.packages("devtools")
+#install.packages("stringr")
+#install.packages("maps")
+#devtools::install_github("wmurphyrd/fiftystater")
 
 library(ggplot2)
 library(dplyr)
@@ -33,28 +33,26 @@ library(rgdal)
 #This data set is found in the fiftystater library
 #I used this one to get alaska and hawaii
 
-#Create test.states, change all lowercase ID t upper state
+setwd("C:/Users/75LPYOTT/Desktop/PRESS")
+
+#Create test.states, change all lowercase ID to upper case
 test.states <- fifty_states %>%
   mutate(state=toupper(id)) 
 
-#read in data set with voting rights by state
-#I created this from data from the Brennancenter.org
-#Brennan Center for Justice
+
+#This data set contains the 50 US States plus DC
+#Several Yes/No Variables
 
 test.incidents <- read.csv("incidents_by_state.csv", header=TRUE)
 
-#Create test.voting to capitalize state
+#Create test.incidents to capitalize state
 test.incidents <- test.incidents  %>%
   mutate(state=toupper(state)) 
 
 #full join data sets by state
 joined_data <- full_join(test.states, test.incidents, by="state")
 
-##test <- joined_data %>%
-##mutate(col=ifelse(automatic.voter.registration=="yes", "blue", "red"))
-
-##plot US state map, color states red=no and blue=yes 
-##map 1: automatic voter registration
+#US map designating states where attacks on the press occured
 mapCOP <- ggplot(data=joined_data)+
   geom_polygon(aes(x=long, y=lat, fill=police_attack, group=group), 
                color="white")+
